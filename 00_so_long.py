8e847afa97f7751f8cfbc6c87eb6d07951ff3299
YuraT <ytatisch@gmail.com>
2019-09-28 09:32:38 -0700

    completed exercise 00

---
+++ 00_so_long.py
@@ -0,0 +1,35 @@
+def main():
+    animal_plural = "cats"
+    emotion = "depressed"
+    character_trait = "cooperation"
+    adjective = "complicated"
+    animal_2 = "tigers"
+
+    print('''So long and thanks for all the {}
+So {} that it should come to this
+We tried to warn you all but oh dear?
+
+You may not share our {}
+Which might explain your disrespect
+For all the {} wonders that
+Grow around you
+
+So long, so long and thanks
+For all the {}
+
+The world's about to be destroyed
+There's no point getting all {}
+Lie back and let the planet dissolve
+
+Despite those nets of {} fleets
+We thought that most of you were {}
+Especially {} tots and your
+Pregnant women
+
+So long, so long, so long, so long, so long
+So long, so long, so long, so long, so long
+
+So long, so long and thanks
+For all the {}'''.format(animal_plural, emotion, character_trait, adjective, animal_plural, emotion, animal_2, emotion, adjective, animal_plural))
+
+main()
